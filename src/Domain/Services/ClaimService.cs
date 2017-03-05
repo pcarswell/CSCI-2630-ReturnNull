7aeedda3eb69fe0b797bed020e598b3b18b6e468
jdmac020 <jmacgregor@student.cscc.edu>
2017-03-05 17:38:28 -0500

    Backend wiring--MS3 Reqs Met
    Entity Framework completed for Claims. No mechanism for adding claim
    info--yet. Added temporary try-catches to allow empty ClaimDomainModels
    to flow back to controller without breaking program.

---
+++ src/Domain/Services/ClaimService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EDeviceClaims.Interactors;
+
+namespace EDeviceClaims.Domain.Services
+{
+
+    public interface IClaimService
+    {
+        ClaimDomainModel StartClaim(Guid policyId);
+        ClaimDomainModel ViewClaim(Guid policyId);
+    }
+
+    public class ClaimService : IClaimService
+    {
+        private IGetPolicyInteractor _getPolicyInteractor;
+
+        private IGetPolicyInteractor GetPolicyInteractor
+        {
+            get { return _getPolicyInteractor ?? (_getPolicyInteractor = new GetPolicyInteractor()); }
+            set { _getPolicyInteractor = value; }
+        }
+
+        private IGetClaimInteractor _getClaimInteractor;
+
+        private IGetClaimInteractor GetClaimInteractor
+        {
+            get { return _getClaimInteractor ?? (_getClaimInteractor = new GetClaimInteractor()); }
+            set { _getClaimInteractor = value; }
+        }
+
+        public ClaimDomainModel StartClaim(Guid policyId)
+        {
+            var policy = GetPolicyInteractor.GetById(policyId);
+
+            if (policy == null) throw new ArgumentException("There is no policy for that ID.");
+
+            // Check for existing claim
+            var existingClaim = GetClaimInteractor.GetClaimById(policyId);
+
+            // TODO:Create new claim
+
+            // currently returns empty model regardless
+            // will eventually need to either return existing or new claim
+            return new ClaimDomainModel();
+        }
+
+        public ClaimDomainModel ViewClaim(Guid policyId)
+        {
+            var policy = GetPolicyInteractor.GetById(policyId);
+
+            if (policy == null) throw new ArgumentException("There is no policy for that ID.");
+
+            var existingClaim = GetClaimInteractor.GetClaimById(policyId);
+
+            // returns new claim model regardless
+            // will eventually need to return existing claim data or error handle
+            return new ClaimDomainModel();
+        }
+    }
+}
---
+++ src/WebUi/Models/ClaimViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EDeviceClaims.Domain.Services;
+
+namespace EDeviceClaims.WebUi.Models
+{
+    public class ClaimViewModel
+    {
+        public ClaimViewModel(ClaimDomainModel domainModel)
+        {
+            
+        }
+    }
+}
