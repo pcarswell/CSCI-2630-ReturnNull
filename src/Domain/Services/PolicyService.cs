7d3a8eeaecf06d9174d5357a4a5d42a281ab90b5
jdmac020 <jmacgregor@student.cscc.edu>
2017-03-05 11:11:13 -0500

    Detail View
    Added "Detail" buttons to "Devices" page. Added "Details" to
    DeviceController. Created "Details" view under Devices. Created logic to
    view device details.

--- src/Domain/Services/PolicyService.cs
+++ src/Domain/Services/PolicyService.cs
@@ -7,26 +7,37 @@ using EDeviceClaims.Interactors;
 
 namespace EDeviceClaims.Domain.Services
 {
-  public interface IPolicyService
-  {
-    IEnumerable<PolicyDomainModel> GetByUserId(string userId);
-  }
-
-  public class PolicyService : IPolicyService
-  {
-    private IGetPolicyInteractor _getPolicyInteractor;
-
-    private IGetPolicyInteractor GetPolicyInteractor
+    public interface IPolicyService
     {
-      get { return _getPolicyInteractor ?? (_getPolicyInteractor = new GetPolicyInteractor()); }
-      set { _getPolicyInteractor = value; }
+        IEnumerable<PolicyDomainModel> GetByUserId(string userId);
+        PolicyDomainModel GetById(Guid policyId);
     }
 
-    public IEnumerable<PolicyDomainModel> GetByUserId(string userId)
+    public class PolicyService : IPolicyService
     {
-      var policyEntities = GetPolicyInteractor.GetByUserId(userId);
+        private IGetPolicyInteractor _getPolicyInteractor;
 
-      return policyEntities.Select(policyEntity => new PolicyDomainModel(policyEntity)).ToList();
+        private IGetPolicyInteractor GetPolicyInteractor
+        {
+            get { return _getPolicyInteractor ?? (_getPolicyInteractor = new GetPolicyInteractor()); }
+            set { _getPolicyInteractor = value; }
+        }
+
+        public IEnumerable<PolicyDomainModel> GetByUserId(string userId)
+        {
+            var policyEntities = GetPolicyInteractor.GetByUserId(userId);
+
+            return policyEntities.Select(policyEntity => new PolicyDomainModel(policyEntity)).ToList();
+        }
+
+        public PolicyDomainModel GetById(Guid policyId)
+        {
+            var entity = GetPolicyInteractor.GetById(policyId);
+
+            if (entity == null) return null;
+            
+            return new PolicyDomainModel(entity);
+
+        }
     }
-  }
 }
