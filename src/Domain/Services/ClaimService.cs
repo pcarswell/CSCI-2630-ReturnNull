f37a1adeb327eac8162f1fb18dcf36141578b115
John MacGregor <jmacgregor@student.cscc.edu>
2017-04-04 19:56:07 -0400

    Revert "Development"

--- src/Domain/Services/ClaimService.cs
+++ src/Domain/Services/ClaimService.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using EDeviceClaims.Domain.Models;
 using EDeviceClaims.Interactors;
 
 namespace EDeviceClaims.Domain.Services
--- src/WebUi/Scripts/indexJavaScript.js
+++
@@ -1,6 +0,0 @@
-﻿$(document).ready(function () {
-
-}
-
-//(/Content/Images/image.jpg)
-);
