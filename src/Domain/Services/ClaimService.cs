7a037087cb1f8ef7cbfd99dff4f66d4b3255325e
jdmac020 <jmacgregor@student.cscc.edu>
2017-04-11 11:25:34 -0400

    Imported ClaimDomain Reference

--- src/Domain/Services/ClaimService.cs
+++ src/Domain/Services/ClaimService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EDeviceClaims.Domain.Models;
 using EDeviceClaims.Interactors;
 
 namespace EDeviceClaims.Domain.Services
